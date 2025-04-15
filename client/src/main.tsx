1d382a321eddcde7808a033b77119f92ffa7dc72
SMX541 <sarveshmuttepawar54@gmail.com>
2025-04-15 10:40:33 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+// If the file is named "app.tsx"
+import App from './app';
+
 import "./index.css";
 import { Toaster } from "@/components/ui/toaster";
 
